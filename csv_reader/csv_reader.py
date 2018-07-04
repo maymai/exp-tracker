1d390db296c6ebf70f8528c923b9741ddbbcc1cc
maymai <mmaiafiorini@gmail.com>
2018-07-04 01:44:49 +0100

    ???

--- csv_reader/csv_reader.py
+++ csv_reader/csv_reader.py
@@ -1,73 +1,173 @@
+"""
+This script takes a csv file and uses its contents to create a list of
+dictionaries representing transactions.
+
+CSV files used are based on nationwide bank statements. Headers are hardcoded
+ on _remove_existing function.
+
+ This script's main function is
+ write_csv - which creates a new csv with all transactions gathered as well as
+ return a list of dictionaries with the same transactions
+"""
 import csv
 import re
-import datetime
+import os
+import collections
 from word_cutter import word_cutter
 
+
 def _read_csv(file):
-    if file[-4] != ".csv":
+    """
+    :param file: string - filepath with or without ".csv"
+    :return: iterator - of rows in the file as dictionaries
+    """
+    if file[-4:] != ".csv":
         file += ".csv"
     return csv.DictReader(open(file))
 
+
+def fix_file(file):
+    """
+    makes a csv file compliant with script
+    removes any rows before initial headers
+    replaces original file with edited one
+    :param file: string - csv filepath
+    """
+    with open(file, newline='') as csvfile:
+        final_list = []
+        r = csv.reader(csvfile, delimiter=',', quotechar='|')
+        start = False
+        while not start:
+            row = r.__next__()
+            if row:
+                if "Date" in row[0]:
+                    start = True
+                    final_list.append(row)
+        for row in r:
+            final_list.append(row)
+    with open(file, "w") as f:
+        for row in final_list:
+            f.write(",".join(row) + "\n")
+
+
 def _fix_headers(transaction_list):
+    """
+    removes whitespace strings from headers and removes unused fields from transaction
+    :param transaction_list: list of dictionaries
+    :return: list of dictionaries with normalised headers, header that contain payee description
+    """
     new_transaction_list = []
+    payee_header = None
     for transaction in transaction_list:
         new_transaction = {}
+        if transaction.get("Description"):
+            remove = "Transaction type"
+            payee_header = "Description"
+        elif transaction.get("Transactions"):
+            remove = "Location"
+            payee_header = "Transactions"
+        elif transaction.get("Name"):
+            remove = None
+            payee_header = "Name"
+        else:
+            return None
         for item in transaction:
-            new_transaction[re.sub("ï»¿", "", item)] = re.sub("Â", "", transaction[item]).lstrip()
+            if item != remove:
+                new_transaction[re.sub("ï»¿", "", item)] = re.sub("Â", "", transaction[item]).lstrip()
         new_transaction_list.append(new_transaction)
-    return new_transaction_list
+    return new_transaction_list, payee_header
+
 
 def _get_transactions_list(file):
+    """
+    reads a file and return a dictionary with header:row values
+    :param file: string - filepath with or without ".csv"
+    :return: list of dictionaries (header:value) for each row on csv file with
+    headers formatted, header that contain payee description
+    """
     transactions = []
     reader = _read_csv(file)
     for row in reader:
         transactions.append(dict(row))
-    transactions = _fix_headers(transactions)
-    return transactions
+    transactions, payee_header = _fix_headers(transactions)
+    return transactions, payee_header
 
-def _remove_existing(new_list, payee_header, old_list=None):
-    old_new_realtionship = {"Date": "Date", "Name": payee_header, "In": "Paid in", "Out": "Paid out"}
-    new_transactions = []
-    for transaction in new_list:
-        new_transaction = {}
-        transaction[payee_header] = word_cutter.format_payee(transaction[payee_header])
-        if old_list:
-            for old in old_list:
-                for h in old_new_realtionship:
-                    if old[h] == transaction[old_new_realtionship[h]]:
-                        break
-                    new_transaction[h] = transaction[old_new_realtionship[h]]
-                if old["Name"] == transaction[payee_header]:
-                    new_transaction["Category"] = old["Category"]
+
+def _merge_transactions(old_transactions, new_transactions):
+    """
+    takes 2 lists of dictionaries, rename headers to match, remove duplicates and
+    return 1 list of dictionaries
+    :param old_transactions: list of dictionaries or string with csv filepath
+        (csv created by this script previously)
+    :param new_transactions: list of dictionaries or string with csv filepath
+        (csv containing new statement for processing)
+    :return: list of dictionaries
+    """
+    # verifying new and old transactions
+    new_payee_header = None
+    if type(old_transactions) == str:
+        old_transactions, old_payee_header = _get_transactions_list(old_transactions)
+    elif type(old_transactions) != list:
+        return None
+    if type(new_transactions) == str:
+        new_transactions, new_payee_header = _get_transactions_list(new_transactions)
+    elif type(new_transactions) != list:
+        return None
+    # change headers to match
+    old_new_realtionship = {"Date": "Date", "Name": new_payee_header, "Paid in": "Paid in", "Paid out": "Paid out"}
+    new_h_trans = []
+    for trans in new_transactions:
+        h_trans = {}
+        for h in old_new_realtionship:
+            h_trans[h] = trans.get(old_new_realtionship[h])
+        new_h_trans.append(h_trans)
+    # format new payees
+    for transaction in new_h_trans:
+        transaction["Name"] = word_cutter.format_payee(transaction["Name"])
+    # create new list with all old transactions and append only new transactions
+    final_transactions = old_transactions.copy()
+    for n in new_h_trans:
+        duplicate = False
+        for existing_trans in final_transactions:
+            if n == {"Name": existing_trans["Name"], "Date": existing_trans["Date"],
+                     "Paid in": existing_trans["Paid in"], "Paid out": existing_trans["Paid out"]}:
+                # have to specify keys since some may have the extra key 'Category'
+                duplicate = True
+                break
+        if not duplicate:
+            final_transactions.append(n)
+    # add category key to existing payees
+    for transaction in final_transactions:
+        if not transaction.get("Pay category"):
+            payee = transaction.get("Name")
+            for t in final_transactions:
+                if t["Name"] == payee and t.get("Pay category"):
+                    transaction["Pay category"] = t["Pay category"]
+                    break
                 else:
-                    new_transaction["Category"] = ""
-        else:
-            for h in old_new_realtionship:
-                new_transaction[h] = transaction[old_new_realtionship[h]]
-                new_transaction["Category"] = ""
-        new_transactions.append(new_transaction)
-    return new_transactions
+                    transaction["Pay category"] = ""
+    return final_transactions
 
-# Make a function to merge transactions into one list
-# Update function below to just write it to a file
 
-def write_csv(new_file, payee_header, old_file=None):
-    if old_file:
-        old_list = _get_transactions_list(old_file)
+def create_final_transaction_list(new_file):
+    """
+    duplicates _merge_transactions looks for previously created csv and final list is
+    written on csv file
+    :param new_file: string - bank statement csv filepath
+    :return: list of dictionaries
+    """
+    filename = "final_transactions.csv"
+    if not os.path.isfile(filename):
+        old_file = []
     else:
-        old_list = []
-    new_list = _get_transactions_list(new_file)
-    new_list = _remove_existing(new_list, payee_header, old_list)
-    filename = datetime.datetime.utcnow().strftime("%Y%m%d_%H%M") + ".xls"
-    with open(filename, "w", newline='') as file:
-        if old_list:
-            headers = old_list[0].keys()
-        else:
-            headers = new_list[0].keys()
-        writer = csv.DictWriter(file, fieldnames=headers)
-        writer.writeheader()
-        for transaction in old_list:
-            writer.writerow(transaction)
-        for transaction in new_list:
-            writer.writerow(transaction)
-    return old_list + new_list
+        old_file = filename
+    final_transactions = _merge_transactions(old_file, new_file)
+    if final_transactions:
+        with open(filename, "w", newline='') as file:
+            headers = list(final_transactions[0].keys())
+            headers.sort()
+            writer = csv.DictWriter(file, fieldnames=headers)
+            writer.writeheader()
+            for transaction in final_transactions:
+                writer.writerow(transaction)
+    return final_transactions
--- word_cutter/word_cutter.py
+++ word_cutter/word_cutter.py
@@ -1,13 +1,43 @@
+"""
+This script was created to format payee names on a bank statement to make them
+easier to be stored in a database. It removes part of the string that may alter the
+name of the payee reducing the chance of duplicates.
+
+This script contains 2 main functions:
+format_payees_in_file
+format_payee
+
+Functions starting with "_" are support functions to be used by format_payee
+"""
 import csv
 import re
 from difflib import SequenceMatcher as sm
 
+
 def _get_similarity(a, b):
+    """
+    :param a: string
+    :param b: string
+    :return: percentage similarity rate between a and b
+    """
     return sm(None, a, b).ratio()
 
+
 def _cut_by_word(word, cut, check=None, prefix=True, start=0, end=0):
-    if not check: check = cut
-    if not end: end = len(word)
+    """
+    removes part of a sentence or word
+    :param word: string
+    :param cut: string - part of the string to be removed
+    :param check: string - part of the string that must exist in order to remove cut
+    :param prefix: boolean -  true = cut is at the start of word, false = cut is at the end of word
+    :param start: integer - index of starting point of word
+    :param end: integer - index of ending point of word
+    :return: string - word - cut if check = true
+    """
+    if not check:
+        check = cut
+    if not end:
+        end = len(word)
     if prefix:
         if check in word[start:end]:
             return word[:word.index(cut)]
@@ -16,14 +46,27 @@ def _cut_by_word(word, cut, check=None, prefix=True, start=0, end=0):
             return word[word.index(cut) + 1:]
     return word
 
+
 def _cut_by_length(words, length):
+    """
+    removes small words from sentences
+    :param words: string - sentence to have words removes from
+    :param length: integer - max number of letters in words to be removed
+    :return: string - words - word where len(word) <= length
+    """
     new_words = []
     for word in words:
         if len(word) > length:
             new_words.append(word)
     return " ".join(new_words)
 
+
 def _cut_by_numbers(word):
+    """
+    removes integers from string
+    :param word: string
+    :return: string - word - numbers
+    """
     number = ""
     for char in word:
         try:
@@ -39,7 +82,13 @@ def _cut_by_numbers(word):
                 return word[:word.index(number)]
     return word
 
+
 def _cut_by_place(word):
+    """
+    removes words fromstring that matches words on list
+    :param word: string
+    :return: string - word - places
+    """
     words = word.split()
     last_word = words[-1]
     places = ["cardiff", "porth", "caerphilly", "ponty", "pontypridd",
@@ -50,7 +99,15 @@ def _cut_by_place(word):
             last_word = words[-1]
     return " ".join(words)
 
+
 def _add_individual_items_to_list(list, item, similarity=0.75):
+    """
+    identifies if item is already on list and appends it if not
+    :param list: list - original list of items
+    :param item: string (hashable element) - item to be added to list
+    :param similarity: float - number between 0 and 1, 1 being identical
+    :return: list - with new item appended or not
+    """
     if not list:
         list.append(item)
     elif item not in list:
@@ -63,7 +120,14 @@ def _add_individual_items_to_list(list, item, similarity=0.75):
             list.append(item)
     return list
 
+
 def format_payees_in_file(file, header=None):
+    """
+    creates a list of strings from a column on a csv file
+    :param file: string - csv filepath
+    :param header: optional string - the header of the csv file that contains the words to be stripped and listed
+    :return: list of strings
+    """
     if file[-4:] != ".csv":
         file += ".csv"
     final_list = []
@@ -81,10 +145,27 @@ def format_payees_in_file(file, header=None):
     final_list = sorted(final_list)
     return final_list
 
-def _cut_bad_characters(word):
-    return re.sub(r"[\W]", "", word)
+
+def _cut_bad_characters(words):
+    """
+    removes expressions from string
+    :param word: string
+    :return: string - unformatted
+    """
+    word_list = words.split()
+    final_word_list = []
+    for word in word_list:
+        final_word = re.sub(r"[\W]", "", word)
+        final_word_list.append(final_word)
+    return " ".join(final_word_list)
+
 
 def format_payee(payee):
+    """
+    used to strip payee names from characters and arrive to a common payee name
+    :param payee: string
+    :return: string formatted
+    """
     formatted_item = payee.lower()
     formatted_item = _cut_by_word(formatted_item, "android")
     formatted_item = _cut_by_word(formatted_item, "*", "paypal", False)
